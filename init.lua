65dc73ea1416aa78ee8d1b8e7767963ce7c2e517
ebj <eric.bjarstal@etu.utc.fr>
2026-06-07 22:43:06 +0200

    edit tabs

--- init.lua
+++ init.lua
@@ -5,4 +5,8 @@ vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
+vim.opt.tabstop = 4       -- how wide a tab character displays
+vim.opt.shiftwidth = 4    -- how wide an indent is (>, <, autoindent)
+vim.opt.expandtab = true  -- insert spaces instead of a tab character
+
 require('config.lazy')
